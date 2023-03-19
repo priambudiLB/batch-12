045ae2997c89814670236819750b421fcbf09990
Priambudi Lintang Bagaskara <priambudibagaskara@gmail.com>
2023-03-19 12:56:22 +0800

    react

--- day-24/my_project/src/App.js
+++ day-24/my_project/src/App.js
@@ -2,10 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('hihi')
+  const text = "Bagas"
+  const num = 10
+  const skills = ["html", "css", "js", "react"]
+  const data = [
+    {
+      name: "react",
+      creator: "meta"
+    },
+    {
+      name: "angular",
+      creator: "google"
+    }
+  ]
   return (
     <div className="App">
+      {console.log("HALOOOOO")}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{text + " " + num}</p>
+        <ul>
+          {skills.map((value, index) => (
+            // kalo pake fungsi .map, jangan lupa pasang attribute "key" di element terluar
+            <li key={index}>{value}</li>
+          ))}
+        </ul>
+        <ol>
+          {data.map((value, index) => (
+            // kalo pake fungsi .map, jangan lupa pasang attribute "key" di element terluar
+            <li key={index}>{value.name + " is created by: " + value.creator}</li>
+          ))}
+        </ol>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
