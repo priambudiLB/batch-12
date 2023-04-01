7404403fcce2aa7feedde32b67d1ebd5701fb8d4
Priambudi Lintang Bagaskara <priambudibagaskara@gmail.com>
2023-04-01 12:04:43 +0800

    crud

--- day-27/crud/src/App.jsx
+++ day-27/crud/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Axios from 'axios'
 
@@ -25,14 +23,6 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <ul>
         {data.map(item => {
           return <li key={item.id}>[{item.id} | {item.name} | {item.age}]</li>
@@ -42,13 +32,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
